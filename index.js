ca432391a907b2b72ccb82a18aac1216675ee853
Raissa Micheline IMPUHWE MANZI <rimpuhwemanzi@gmail.com>
2025-01-06 14:19:31 +0200

    adding the environment file

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+require('dotenv').config()
+
 const express = require('express');
 const app = express();
 app.get('/', (req, res) =>{
     res.send('Hello World!');
 })
-app.listen(3000)
+const port  = process.env.PORT ||3000;
+app.listen(port ,()=>{
+    console.log(`Server running on port ${port}`);
+})
